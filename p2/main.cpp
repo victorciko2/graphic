770030b2882d469fda23bbd4465194ed8940c8a0
David Márquez <davidmarquezcalavia@gmail.com>
2018-11-05 18:59:47 +0100

    Update main.cpp

--- p2/main.cpp
+++ p2/main.cpp
@@ -3,14 +3,23 @@
 #include <fstream>
 #include <vector>
 #include <cmath>
+#include <limits>
 
 using namespace std;
+
+class RGB{
+	float r, g, b;
+	RGB();
+	RGB(float r, float g, float b);
+	
+};
 
 long int rows, columns, c;
 float  MAX = -1;
 vector<vector<float>> matrix;
 vector<vector<float>> matrixLDR;
 string path = "";
+float minValue = std::numeric_limits<float>::infinity(), maxValue = 0;
 
 void fillMatrix(ifstream& f){
 	string s, s2, s3;
@@ -21,7 +30,6 @@ void fillMatrix(ifstream& f){
 		s2 = s.substr(1,3);
 		if(s2.compare("MAX") == 0){
 			MAX = stof(s.substr(5, s.size() - 5), nullptr);
-			cout << MAX << endl;
 		}
 		getline(f, s);
 	}
@@ -32,7 +40,6 @@ void fillMatrix(ifstream& f){
 	for(int i = 0; i < rows; i++){
 		matrix[i].resize(columns * 3);	
 	}
-	int a = 0;
 	for(int i = 0; i < rows; i++){
 		for(int j = 0; j < columns * 3; j++){
 			f >> number;
@@ -40,12 +47,15 @@ void fillMatrix(ifstream& f){
 			if(MAX != -1){
 				matrix[i][j] *= MAX;
 			}
-			if(matrix[i][j] > 1){
-				a++;
+			if(matrix[i][j] < minValue){
+				minValue = matrix[i][j];
+			}
+			if(matrix[i][j] > maxValue){
+				maxValue = matrix[i][j];
 			}
 		}
 	}
-	cout << "numeros mayores que 1 = " << a << endl; 
+	cout << "min = " << minValue << " max = " << maxValue << endl;
 }
 
 void showMatrix(){
@@ -60,11 +70,15 @@ void showMatrix(){
 void clamp(){
 	for(int i = 0; i < rows; i++){
 		for(int j = 0; j < columns * 3; j++){
-			if(matrixLDR[i][j] > 1){
-				matrixLDR[i][j] = 1;
-			} 
+			if(matrixLDR[i][j] > 15000){
+				matrixLDR[i][j] = 15000;
+			}
 		}
 	}
+}
+
+void equalize(){
+
 }
 
 void saveMatrix(){
@@ -73,37 +87,36 @@ void saveMatrix(){
 	o << "#MAX=" << MAX << endl;
 	string aux = path;
 	aux = path.substr(aux.find("/") + 1, path.size() - aux.find("/"));
-	o << "#" << aux << ".ppm" << endl;
+	o << "#" << aux << "LDR.ppm" << endl;
 	o << rows << " " << columns << endl;
-	o << c << endl;
+	o << "255" << endl;
+	//o << c << endl;
 	for(int i = 0; i < rows; i++){
 		for(int j = 0; j < columns * 3; j++){
-			o << (long int) ((matrixLDR[i][j] * c) / MAX) << " ";
-			//o << matrixLDR[i][j] << " "; 
+			o << (long int) ((matrixLDR[i][j] * 255	) / 15000) << " ";
+			//o << (long int) ((matrixLDR[i][j])) << " ";
 		}
 		o << endl;
 	}
 	o.close();
-
 }
 
 int main(){
 	cout << "Choose tone mapper" << endl;
 	cout << "Input path to image: " << flush;
 	cout << endl;
-	path = "hdr-ppm/mpi_office";
+	path = "../../hdr-ppm/seymour_park";
 	//cin >> path;
 	ifstream f;
 	f.open(path+".ppm");
 	if(f.is_open()){
 		fillMatrix(f);
 		matrixLDR = matrix;
-		//clamp();
+		clamp();
 		saveMatrix();
 		f.close();
 	}
 	else{
 		cout << "ERROR: couldnt access to file " << path << endl;
 	}
-
 }
